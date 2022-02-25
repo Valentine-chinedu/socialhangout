35a03bf842761ac7f183562e556284930aaed8f1
okosival <valentine11.dev@gmail.com>
2022-02-25 08:04:51 +0000

    updated parckages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,16 @@
 module.exports = {
-	purge: [
-		"./src/components/feed/Feed.jsx",
-		"./src/components/form/MessageForm.jsx",
-		"./src/components/navbar/NavbarTop.jsx",
-		"./src/components/navbar/SideBar.jsx",
-		"./src/components/layout/Layout.jsx",
-		"./src/components/posts/post/Post.jsx",
-		"./src/components/posts/post/PostButton.jsx",
-		"./src/components/posts/Posts.jsx",
-		"./src/components/profile/Profile.jsx",
-		"./src/components/LoginWithPopup.jsx",
-		"./src/contextProviders/Auth.jsx",
-		"./src/App.js",
-	],
-	darkMode: false, // or 'media' or 'class'
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
 	theme: {
 		extend: {
 			transitionProperty: {
-				width: "width",
-			},
-
-			backgroundColor: {
-				primary: "var(--color-bg-primary)",
-				secondary: "var(--color-bg-secondary)",
-			},
-			textColor: {
-				accent: "var(--color-text-accent)",
-				primary: "var(--color-text-primary)",
-				secondary: "var(--color-text-secondary)",
-			},
-
-			borderColor: {
-				primary: "var(--color-border-primary)",
+				width: 'width',
 			},
 		},
 	},
 	variants: {
 		extend: {
-			opacity: ["disabled"],
+			opacity: ['disabled'],
 		},
 	},
-	plugins: [require("tailwind-scrollbar-hide")],
 };
